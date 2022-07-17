07cdcf40c506f9088c2b22f95e410fcb26f424dd
ericdacal <eric@dacal.es>
2022-07-17 18:29:02 +0200

    Add function for add produt to chart using API

--- src/utils/APIconnector.js
+++ src/utils/APIconnector.js
@@ -7,9 +7,25 @@ export const getItemsFromAPI = async (user) => {
 };
 
 
-export const getItemDetail = async (id) => {
+export const getItemDetailFromAPI = async (id) => {
 	let url = new URL("/api/product/" + id, host)
 	const response = await fetch(url)
 	return await response.json()
 };
+
+export const addProductToCartFromAPI = async (id, colorCode, storageCode) => {
+	let url = new URL("/api/cart/", host)
+	let response = await fetch(url, {
+		method: 'POST',
+		headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+		},
+    	body: JSON.stringify({id: id, colorCode: colorCode, storageCode: storageCode})
+  	});
+ 	const content = await response.json();
+
+  	console.log(content);
+};
+
 
