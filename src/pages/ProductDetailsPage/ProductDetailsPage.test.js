b134211e3258ef652661316b203515269d835e7d
ericdacal <eric@dacal.es>
2022-07-17 19:03:49 +0200

    Fix problem with test and first probe with tests

--- src/pages/ProductDetailsPage/ProductDetailsPage.test.js
+++ src/pages/ProductDetailsPage/ProductDetailsPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import ProductDetailsPage from './ProductDetailsPage'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  render(<ProductDetailsPage />)
+  const linkElement = screen.getByText(/Acer/i)
   expect(linkElement).toBeInTheDocument()
 })
