373d55ec9cddf3264ffcd9553545517a86bd8b9c
ericdacal <eric@dacal.es>
2022-07-23 19:44:59 +0200

    Add unit test for Header, Actions, Description and Item components

--- src/pages/General/Header/Header.test.js
+++ src/pages/General/Header/Header.test.js
@@ -1,16 +1,14 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react'
 import Header from './Header'
 
-
-describe("Fuction to test Header component", () => {
-	const input = {
-		page : 0,
-		itemCount: 1
-	}
-	it('Should have an icon image loaded', async() => {
-		render(<Header page= {input.page} itemCount={input.itemCount} />);
-		const imgItems = await screen.findAllByRole("img");
-		expect(imgItems).toHaveLength(1)
-	});
+describe('Fuction to test Header component', () => {
+  const input = {
+    page: 0,
+    itemCount: 1
+  }
+  it('Should have an icon image loaded', async () => {
+    render(<Header page= {input.page} itemCount={input.itemCount} />)
+    const imgItems = await screen.findAllByRole('img')
+    expect(imgItems).toHaveLength(1)
+  })
 })
-
